11c1c8fa905cd271b0a12d04ffd9ad8db9d8a1df
facepalm <f.acepalm@mail.ru>
2017-04-02 18:47:33 +0300

    Renamed console application.

--- ConsoleApp/Properties/AssemblyInfo.cs
+++ ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mentoring")]
+[assembly: AssemblyTitle("ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mentoring")]
+[assembly: AssemblyProduct("ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21d96b4d-42be-4a7a-a1e0-b87b21718fa2")]
+[assembly: Guid("0847be54-899a-42a9-8d03-11cb264ae6f0")]
 
 // Version information for an assembly consists of the following four values:
 //
