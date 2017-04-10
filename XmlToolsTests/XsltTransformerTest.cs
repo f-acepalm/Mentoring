3a7c353cce6257bca5699c4d791c0b57cac4ad1a
facepalm <f.acepalm@mail.ru>
2017-04-10 22:39:43 +0300

    Added rss and html transformations

---
+++ XmlToolsTests/XsltTransformerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XmlTools;
+using System.IO;
+
+namespace XmlToolsTests
+{
+    [TestClass]
+    public class XsltTransformerTest
+    {
+        private const string Source = @"..\..\books.xml";
+        private const string RssXsltFile = @"..\..\BooksToRSS.xslt";
+        private const string RssResultFile = @"..\..\RssResult.xml";
+        private const string HtmlXsltFile = @"..\..\BooksToHtml.xslt";
+        private const string HtmlResultFile = @"..\..\HtmlResult.html";
+
+        [TestMethod]
+        public void Transform_Rss_WithStreamWriter()
+        {
+            var generator = new XsltTransformer();
+            File.Delete(RssResultFile);
+
+            using (var writer = new StreamWriter(RssResultFile))
+            {
+                generator.Transform(Source, RssXsltFile, writer);
+            }
+
+            Assert.IsTrue(File.Exists(RssResultFile));
+        }
+                
+        [TestMethod]
+        public void Transform_Rss_WithRssResultFile()
+        {
+            var generator = new XsltTransformer();
+            File.Delete(RssResultFile);
+
+            generator.Transform(Source, RssXsltFile, RssResultFile);
+
+            Assert.IsTrue(File.Exists(RssResultFile));
+        }
+
+        [TestMethod]
+        public void Transform_Rss_WithConsoleWriter()
+        {
+            var generator = new XsltTransformer();
+            generator.Transform(Source, RssXsltFile, Console.Out);
+        }
+
+        [TestMethod]
+        public void Transform_Html()
+        {
+            var generator = new XsltTransformer();
+            File.Delete(HtmlResultFile);
+
+            generator.Transform(Source, HtmlXsltFile, HtmlResultFile);
+
+            Assert.IsTrue(File.Exists(HtmlResultFile));
+        }
+    }
+}
