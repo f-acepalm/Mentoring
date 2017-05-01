a26813546afa2e4b84ee74eea56dff9ea16088f8
facepalm <f.acepalm@mail.ru>
2017-05-01 23:59:07 +0300

    Added some tasks

---
+++ 10Operations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _10Operations
+{
+    class Program
+    {
+        private static SemaphoreSlim semaphore;
+
+        public static void Main(string[] arg)
+        {
+            semaphore = new SemaphoreSlim(0, 2);
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => {
+                    Console.WriteLine($"Operation #{Thread.CurrentThread.ManagedThreadId} is waiting for the semaphore.");
+
+                    semaphore.Wait();
+
+                    Console.WriteLine($"Operation #{Thread.CurrentThread.ManagedThreadId} enters the semaphore.");
+
+                    Thread.Sleep(2000);
+
+                    Console.WriteLine($"Operation #{Thread.CurrentThread.ManagedThreadId} releases the semaphore; previous count: {semaphore.Release()}.");
+                });
+
+                threads[i].Start();
+            }
+
+            Thread.Sleep(1000);
+
+            Console.Write("Main thread calls Release(2) --> ");
+            semaphore.Release(2);
+            Console.WriteLine($"{semaphore.CurrentCount} tasks can enter the semaphore.");
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine("Main thread exits.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ContinuationTasks/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinuationTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point1();
+            Point2();
+            Point3();
+            Point4();
+            Console.WriteLine("Done!");
+        }
+
+        private static void Point1()
+        {
+            Task.Run(() =>
+            {
+                Console.WriteLine("Task started");
+                throw new Exception();
+            })
+            .ContinueWith((data) =>
+            {
+                Console.WriteLine("regardless of the result continuation");
+            })
+            .Wait();
+        }
+
+        private static void Point2()
+        {
+            Task.Run(() =>
+            {
+                Console.WriteLine("Task started");
+                throw new Exception();
+            })
+            .ContinueWith((data) =>
+            {
+                Console.WriteLine("OnlyOnFaulted continuation");
+            }, TaskContinuationOptions.OnlyOnFaulted)
+            .Wait();
+        }
+
+        private static void Point3()
+        {
+            Task.Run(() =>
+            {
+                Console.WriteLine("Task started");
+                throw new Exception();
+            })
+            .ContinueWith((data) =>
+            {
+                Console.WriteLine("AttachedToParent continuation");
+            }, TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnFaulted)
+            .Wait();
+        }
+
+        private static void Point4()
+        {
+            Task.Run(() =>
+            {
+                Console.WriteLine("Task started");
+                throw new TaskCanceledException();
+            })
+            .ContinueWith((data) =>
+            {
+                Console.WriteLine("ExecuteSynchronously continuation");
+            }, TaskContinuationOptions.OnlyOnCanceled | TaskContinuationOptions.ExecuteSynchronously )
+            .Wait();
+        }
+    }
+}
